106c9189e6a45d273b9b137d4558ba77c804092d
Maheen Fatima <s.maheenfatima16@gmail.com>
2025-08-11 10:55:43 +0500

    navbar added

--- __manifest__.py
+++ __manifest__.py
@@ -3,14 +3,17 @@
     'author': 'eaaxeesoft',
     'category': 'Website',
     'license': 'LGPL-3',
-    'version': '17.0.1.0.0',
+    'version': '1.0',
     'data': [
         'views/hero_section.xml',
+        # 'views/assets.xml',        # Removed to avoid website.assets_frontend inherit error
     ],
+    'depends': ['website'],
     'assets': {
         'web.assets_frontend': [
             '/restaurant_custom/static/src/css/hero.css',
             '/restaurant_custom/static/src/js/hero.js',
+            '/restaurant_custom/static/src/css/navbar.css',
         ],
     },
     'installable': True,
